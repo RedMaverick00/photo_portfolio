81ea9ff521916db5130c6b1b5cf6ce25fb40d773
RedMaverick00 <ctmd.school@gmail.com>
2019-05-06 19:34:08 -0500

    Full light/dark mode
    -only for front page

--- js.js
+++ js.js
@@ -1,11 +1,24 @@
 
 function dankMode() {
     document.body.style.backgroundColor="black";
+    myTag=document.getElementsByClassName('my-color');
+    myTag2=document.getElementsByClassName('opp-my-color');
+    for(i=0; i<  myTag.length; i++ )
+        myTag[i].style.backgroundColor="#111";
 
+    for(i=0; i<  myTag2.length; i++ )
+        myTag2[i].style.color="#eee";
   }
 
 
   function lightMode() {
     document.body.style.backgroundColor="white";
 
+    myTag=document.getElementsByClassName('my-color');
+    myTag2=document.getElementsByClassName('opp-my-color');
+    for(i=0; i<  myTag.length; i++ )
+        myTag[i].style.backgroundColor="#fff";
+
+        for(i=0; i<  myTag2.length; i++ )
+        myTag2[i].style.color="#000";
   }
